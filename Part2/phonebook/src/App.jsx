4ac12079e004bf55b3bc0762aa9e587c804e529f
Puriwat <uglypow@gmail.com>
2024-02-18 01:16:12 +0700

    Finished phonebook step 4

--- Part2/phonebook/src/App.jsx
+++ Part2/phonebook/src/App.jsx
@@ -1,34 +1,70 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import Filter from './components/Filter'
+import PersonForm from './components/personForm'
 
-function App() {
-  const [count, setCount] = useState(0)
+const App = () => {
+  const [persons, setPersons] = useState([
+    { name: 'Arto Hellas', number: '040-123456', id: 1 },
+    { name: 'Ada Lovelace', number: '39-44-5323523', id: 2 },
+    { name: 'Dan Abramov', number: '12-43-234345', id: 3 },
+    { name: 'Mary Poppendieck', number: '39-23-6423122', id: 4 }
+  ])
+  const [newName, setNewName] = useState('')
+  const [newNumber, setNewNumber] = useState('')
+  const [searchPerson, setSearchPerson] = useState('')
+  const filteredPerson = persons.filter(persons =>
+    persons.name.toLowerCase().includes(searchPerson.toLowerCase())
+  );
+
+  const addPersons = (event) => {
+    event.preventDefault()
+    const tempNewName = {
+      name: newName,
+      number: newNumber,
+      id: persons.length + 1,
+    }
+
+    if (!checkExistName(persons, tempNewName)) {
+      setPersons(persons.concat(tempNewName))
+      setNewName('')
+      setNewNumber('')
+    } else
+      alert(`${newName} is already added to phonebook`)
+  }
+  const checkExistName = (persons, newPerson) => {
+    return persons.some(item => item.name === newPerson.name);
+  }
+
+  const handleNameChange = (event) => {
+    setNewName(event.target.value)
+  }
+
+  const handleNumberChange = (event) => {
+    setNewNumber(event.target.value)
+  }
+
+  const handleSearchChange = (event) => {
+    setSearchPerson(event.target.value)
+  }
+
+
+
 
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <div>
+      <h2>Phonebook</h2>
+      <Filter searchPerson={searchPerson}
+        handleSearchChange={handleSearchChange} />
+      <h2>Add a new</h2>
+      <PersonForm newNumber={newNumber}
+        newName={newName}
+        handleNameChange={handleNameChange}
+        handleNumberChange={handleNumberChange}
+        addPersons={addPersons} />
+      <h2>Numbers</h2>
+      {filteredPerson.map(person =>
+        <div key={person.id}>{person.name} {person.number}</div>)}
+    </div>
   )
 }
 

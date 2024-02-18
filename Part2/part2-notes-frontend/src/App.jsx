e0c8306f332558df13b67d00e8c66eed71928791
Puriwat <uglypow@gmail.com>
2024-02-18 23:42:36 +0700

    App can now fetch data from json server

--- Part2/part2-notes-frontend/src/App.jsx
+++ Part2/part2-notes-frontend/src/App.jsx
@@ -1,12 +1,23 @@
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
+import axios from 'axios'
 import Note from './components/Note'
 
-const App = (props) => {
-  const [notes, setNotes] = useState(props.notes)
+const App = () => {
+  const [notes, setNotes] = useState([])
   const [newNote, setNewNote] = useState(
     'a new note...'
-  ) 
+  )
   const [showAll, setShowAll] = useState(true)
+
+  useEffect(() => {
+    console.log('effect')
+    axios
+      .get('http://localhost:3001/notes')
+      .then(response => {
+        console.log('promise fulfilled')
+        setNotes(response.data)
+      })
+  }, [])
 
 
   const addNote = (event) => {
@@ -16,7 +27,7 @@ const App = (props) => {
       important: Math.random() > 0.5,
       id: notes.length + 1,
     }
-  
+
     setNotes(notes.concat(noteObject))
     setNewNote('')
   }
@@ -35,21 +46,21 @@ const App = (props) => {
       <h1>Notes</h1>
       <div>
         <button onClick={() => setShowAll(!showAll)}>
-          show {showAll ? 'important' : 'all' }
+          show {showAll ? 'important' : 'all'}
         </button>
       </div>
       <ul>
-        {notesToShow.map(note => 
+        {notesToShow.map(note =>
           <Note key={note.id} note={note} />
         )}
       </ul>
       <form onSubmit={addNote}>
-      <input
+        <input
           value={newNote}
           onChange={handleNoteChange}
         />
         <button type="submit">save</button>
-      </form> 
+      </form>
     </div>
   )
 }

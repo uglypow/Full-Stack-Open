7987a1d2e668219243a641e76f1e3fd702b4df46
Puriwat <uglypow@gmail.com>
2024-02-19 01:07:57 +0700

    add toggleImportanceOf

--- Part2/part2-notes-frontend/src/App.jsx
+++ Part2/part2-notes-frontend/src/App.jsx
@@ -41,8 +41,14 @@ const App = () => {
     setNewNote(event.target.value)
   }
 
-  const toggleImportanceOf = (id) => {
-    console.log('importance of ' + id + ' needs to be toggled')
+  const toggleImportanceOf = id => {
+    const url = `http://localhost:3001/notes/${id}`
+    const note = notes.find(n => n.id === id)
+    const changedNote = { ...note, important: !note.important }
+  
+    axios.put(url, changedNote).then(response => {
+      setNotes(notes.map(n => n.id !== id ? n : response.data))
+    })
   }
 
   const notesToShow = showAll

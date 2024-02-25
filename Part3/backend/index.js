7b3452e90b3b1ec5721958803aa5097fd9277905
Puriwat <uglypow@gmail.com>
2024-02-25 17:30:02 +0700

    added production build

--- Part3/backend/index.js
+++ Part3/backend/index.js
@@ -1,6 +1,9 @@
 const express = require('express')
 const app = express()
+const cors = require('cors')
 
+app.use(express.static('dist'))
+app.use(cors())
 app.use(express.json())
 
 let notes = [
@@ -20,6 +23,15 @@ let notes = [
         important: true
     }
 ]
+
+const requestLogger = (request, response, next) => {
+    console.log('Method:', request.method)
+    console.log('Path:  ', request.path)
+    console.log('Body:  ', request.body)
+    console.log('---')
+    next()
+}
+app.use(requestLogger)
 
 app.get('/', (request, response) => {
     response.send('<h1>Hello World!</h1>')
@@ -75,7 +87,7 @@ app.post('/api/notes', (request, response) => {
     response.json(note)
 })
 
-const PORT = 3001
+const PORT = process.env.PORT || 3001
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`)
 })

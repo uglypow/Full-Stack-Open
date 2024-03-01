719f661fef01f44c755d6d494ccc09398ccbfaab
Puriwat <uglypow@gmail.com>
2024-03-01 22:24:14 +0700

    finished 'Note on exports'

---
+++ Part4/backend/utils/logger.js
@@ -0,0 +1,13 @@
+const info = (...params) => {
+    console.log(...params)
+}
+
+const error = (...params) => {
+    console.error(...params)
+}
+
+const logger = {
+    info, error
+}
+
+module.exports = logger

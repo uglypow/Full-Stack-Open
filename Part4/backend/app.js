f575439aad81786c6be261a3eb5982ee6e36fec1
Puriwat <uglypow@gmail.com>
2024-04-18 00:37:01 +0700

    Limiting creating new notes to logged-in users

--- Part4/backend/app.js
+++ Part4/backend/app.js
@@ -2,7 +2,9 @@ require('express-async-errors')
 const express = require('express')
 const app = express()
 const cors = require('cors')
+const usersRouter = require('./controllers/users')
 const notesRouter = require('./controllers/notes')
+const loginRouter = require('./controllers/login')
 const mongoose = require('mongoose')
 const config = require('./utils/config')
 const logger = require('./utils/logger')
@@ -27,7 +29,10 @@ app.use(cors())
 app.use(express.json())
 app.use(middleware.requestLogger)
 
+app.use('/api/users', usersRouter)
 app.use('/api/notes', notesRouter)
+app.use('/api/login', loginRouter)
+
 
 // handler of requests with unknown endpoint
 app.use(middleware.unknownEndpoint)
